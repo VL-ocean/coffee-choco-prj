14db55511c141d3e1b8f5498aec0f049e310cf7b
Viktoriia Liakhovetska <vliakhovetska@gmail.com>
2024-08-29 10:38:04 +0000

    Deployment

--- coffee_choco/settings.py
+++ coffee_choco/settings.py
@@ -32,7 +32,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     "8000-vlocean-coffeechocoprj-7p4q8sqje4d.ws.codeinstitute-ide.net",
-    ".herokuapp.com",
+    "https://coffee-choco-blog-7ff7b0b383df.herokuapp.com/",
 ]
 
 # Application definition
