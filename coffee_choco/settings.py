1fa147026c41416580fd850139796252525c38e3
Viktoriia Liakhovetska <vliakhovetska@gmail.com>
2024-08-29 12:10:58 +0000

    Create home app and base template

--- coffee_choco/settings.py
+++ coffee_choco/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps
+    'home',
     'blog',
 ]
 
@@ -62,7 +65,9 @@ ROOT_URLCONF = 'coffee_choco.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
