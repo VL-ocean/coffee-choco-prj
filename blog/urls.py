4219837db4e89511934a618815f566b5b2d15997
Viktoriia Liakhovetska <vliakhovetska@gmail.com>
2024-09-05 13:09:32 +0000

    Add delete functionality for posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AddPost, Posts, PostDetail
+from .views import AddPost, Posts, PostDetail, DeletePost
 
 urlpatterns = [
-    path("", AddPost.as_view(), name="add_post"),
-    path("blog/", Posts.as_view(), name="blog"),
+    path("add/", AddPost.as_view(), name="add_post"),
     path("<slug:slug>/", PostDetail.as_view(), name="post_detail"),
+    path("delete/<slug:pk>/", DeletePost.as_view(), name="delete_post"),
+    path("", Posts.as_view(), name="blog"),
 ]
